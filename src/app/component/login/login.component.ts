ce595d31d7f0db56f08c2c82c386b9ec9dec1d52
deshnishant <deshnishant0603@gmail.com>
2024-01-15 16:02:00 +0530

    first commit

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
+
+  credentials={
+    username:'',
+    password:''
+  }
+
+  constructor(){ }
+
+   ngOnInit(): void {}
+
+   onSubmit() {
+    // console.log("Form is submitted");
+    if((this.credentials.username!='' && this.credentials.password!='') && (this.credentials.username!=null && this.credentials.password!=null))
+    {
+      console.log("We have to submit the form to server");
+    }
+
+    else {
+      ("We have to provide the values");
+    }
+   }
 
 }
+
